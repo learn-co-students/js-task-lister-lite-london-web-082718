d12fb20d797b92d7ea7552a564cf4f71ef6c3839
github username <github email address>
2018-10-16 10:15:38 +0100

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,22 @@
 class Task {
+    constructor (taskData) {
+        this.name = taskData.name
+        this.id = taskData.id
+    }
+
+
+    render () {
+        if (this.taskEl) return this.taskEl
+
+        const taskItem = document.createElement('li')
+        taskItem.innerHTML = `<h2>${this.name}</h2> `
+
+        this.taskEl = taskItem
+
+        return this.taskEl
+    }
+
+    remove () {
+        this.taskEl.remove()
+    }
 }
