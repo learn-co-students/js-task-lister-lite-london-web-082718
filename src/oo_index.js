0d7a75705cac1a65f339e1fe58396b168dc198ef
thexyman <makdaa1@gmail.com>
2018-10-19 16:29:07 +0100

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,79 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+
+  // initialize taskList class
+  // const taskList = new TaskList();
+
+  const form = document.getElementById('create-task-form')
+  const inputEl = document.getElementById('new-task-description')
+  const listEl = document.getElementById('tasks')
+  const priorityEl = document.getElementById('priority-selection')
+
+
+  form.addEventListener("submit", event => {
+      event.preventDefault()
+      const text = inputEl.value
+      const priority = priorityEl.value
+      TaskList.addTaskToList(text, priority)
+      // addListItem(text)
+      // inputEl.value = "" >> good for multiple input fields 
+      event.target.reset()
+    })
+
+// End of   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Get form and relevant input fields
+// const form = document.getElementById('create-task-form')
+// const taskDescription = document.getElementById('new-task-description')
+// const deleteBtn = document.querySelectorAll('.removeBtn')
+
+//  //ul where new tasks will live on the DOM
+//  const listDiv = document.getElementById('tasks')
+
+// //  Create function to render apps which we can call when the eventlistener below
+// //  is activated
+
+// const renderAllTasks = function() {
+//    listDiv.innerHTML = taskList.renderTasks();
+// }
+
+//  //Add event listener to add button
+// form.addEventListener("submit", event => {
+//   event.preventDefault()
+//   console.log('hello')
+//   taskList.createNewTask(taskDescription.value)
+//   event.target.reset()
+//   renderAllTasks()
+// })
+
+  //   // Iterate all nodes
+  //   deleteBtn.forEach(btn => {
+  //   // Attach event listener. Note to preserve the button this-reference
+  //   // by using the non-shorthand function
+  //   btn.addEventListener('click', function() {
+  //     console.log('Click!')
+  //       let li = this.parentNode
+  //       li.remove()
+  //   })
+  //   })
+
+  // })
+      
+      
--- src/task.js
+++ src/task.js
@@ -1,2 +1,80 @@
+
+// Creates an individual task
+// Is the model
+
 class Task {
+
+    constructor(text, id, priority) {
+        this.text = text
+        this.id = id 
+        this.priority = priority
+    }
+
+    render () {
+        const listItemEl = document.createElement('li')
+        listItemEl.id = `todo-item-${this.id}`
+        
+        listItemEl.className = this.priority
+        listItemEl.innerHTML = `
+            ${this.text}
+            <button id='delete-button'>X</button>
+        `
+        this.taskEl = listItemEl
+        // debugger 
+        return listItemEl
+        
+    }
+
+    remove () {
+        this.taskEl.remove()
+    } 
+
+
+
+// const addListItem = text => {
+//     const listItemEl = documentCreateElement('li')
+    // listItemEl.id = `todo-item-${counter++}` no longer needed 
+//     listItemEl.innerHTML = `
+//         ${text}
+//         <button id='delete-button'>X<button>
+//     `
+//     const buttonEl = listItemEl.querySelector('#button-button')
+//     buttonEl.addEventListener('click', event => {
+//         listItemEl.remove()
+//     })
+//     listEl.appendChild(listItemEl)
+// }
+
+
+
+
 }
+
+
+
+
+
+
+    // display () {
+    //     this.element = document.createElement('div')
+    //     this.element.innerHTML = this.render()
+    //     return this.element 
+    // }
+
+    // render() {
+
+    //     return `
+    //     <p>
+    //     ${this.description} 
+    //     <button class="delete" data-id=${this.id}>Delete</button>
+    //     </P>
+    //     `
+    //   }
+
+    // remove () {
+    //     this.element.remove()
+    // } 
+
+
+
+
